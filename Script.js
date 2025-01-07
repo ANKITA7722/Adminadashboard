2ed55822963cce90625dd871c227062fbb6fa87a
Ankita <bobdeankita24@gmail.com>
2025-01-07 19:57:57 -0330

    work on create  responsive

--- Script.js
+++ Script.js
@@ -1,3 +1,20 @@
-$('#menu-btn').click(function(){
-    $('#menu').toggleClass("active");
-})
+const sidebar = document.querySelector('.sidebar');
+const toggleBtn = document.querySelector('#toggle-btn');
+const closeBtn = document.querySelector('#close-btn');
+const mainContent = document.querySelector('.main-content');
+
+
+toggleBtn.addEventListener('click', () => {
+    sidebar.classList.toggle('hidden');
+    mainContent.classList.toggle('shrink');
+});
+
+
+closeBtn.addEventListener('click', () => {
+    sidebar.classList.add('hidden');
+    mainContent.classList.add('shrink');
+});
+
+
+
+
